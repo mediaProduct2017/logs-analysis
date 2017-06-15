3f4100ccc23882e490f0fa86835f331d99df59f5
arfu <arfu.guo@gmail.com>
2017-06-14 18:31:11 -0700

    Finish the project

---
+++ analysis.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+# coding: utf-8
+
+from reporting import Reporting
+
+DBNAME = 'news'
+
+analyst = Reporting(DBNAME)
+analyst.create_view()
+analyst.answer_question1()
+result1 = analyst.cur.fetchall()
+print 'Finish Question 1'
+analyst.answer_question2()
+result2 = analyst.cur.fetchall()
+print 'Finish Question 2'
+analyst.answer_question3()
+result3 = analyst.cur.fetchall()
+print 'Finish Question 3'
+analyst.close()
+
+with open('result.txt', 'w') as a_file:
+    a_file.write('The page views for 3 most popular articles\n\n')
+    for row in result1:
+        a_file.write(row[0] + ': ' + str(row[1]) + '\n')
+    a_file.write('\nThe page views for most popular authors\n\n')
+    for row in result2:
+        a_file.write(row[0] + ': ' + str(row[1]) + '\n')
+    a_file.write('\nThe days on which the error rate is more than 1%\n\n')
+    for row in result3:
+        a_file.write(str(row[0])[0:10] + ': ' + str(row[1]) + '\n')
---
+++ reporting.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python2
+# coding: utf-8
+#
+# A reporting tool for web logs in need of a database.
+
+import psycopg2
+
+
+class Reporting(object):
+
+    def __init__(self, dbname):
+        self.db = psycopg2.connect(database=dbname)
+        self.cur = self.db.cursor()
+
+    def close(self):
+        try:
+            self.db.close()
+        except AttributeError:
+            print "Reporting instance has no attribute 'db'"
+
+    def view_question1(self):
+        query = ("create view path_num as "
+                 "select path, count(*) as num "
+                 "from (select * from log where path!='/') as temp "
+                 "group by path "
+                 "order by num desc "
+                 "limit 3")
+        # print query
+        self.cur.execute(query)
+
+    def answer_question1(self):
+        query = ("select title, num "
+                 "from articles right join path_num "
+                 "on path_num.path like CONCAT('%', articles.slug, '%')")
+        self.cur.execute(query)
+
+    def view_question2(self):
+        query = ("create view log_author as " 
+                 "select name, path "
+                 "from authors, log, articles "
+                 "where authors.id=articles.author " 
+                 "and log.path like CONCAT('%', articles.slug, '%')")
+        self.cur.execute(query)
+
+    def answer_question2(self):
+        query = ("select name, count(*) as num "
+                 "from log_author "
+                 "group by name "
+                 "order by num desc")
+        self.cur.execute(query)
+
+    def view_question3(self):
+        query = ("create view rate_table as "
+                 "select date_requests.date, CAST(date_errors.num as FLOAT) / date_requests.num as error_rate "
+                 "from date_requests, date_errors "
+                 "where date_requests.date=date_errors.date")
+        self.cur.execute(query)
+
+    def answer_question3(self):
+        query = ("select date, CAST(error_rate as DECIMAL(4,3)) "
+                 "from rate_table where error_rate>0.01")
+        self.cur.execute(query)
+
+    def create_view(self):
+        self.view_question1()
+        self.view_question2()
+        self.view_question3()
